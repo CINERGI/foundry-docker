d654128117f8cfd30e3c3f516a595d435b2a8553
valentinedwv <david.valentine@gmail.com>
2019-03-16 15:33:11 -0700

    Add stopwords. May put them in a separate repo and pull Updated paths in config-spec.yml's

--- foundry_stack/foundry/bin/docker_consumer_head.sh
+++ foundry_stack/foundry/bin/docker_consumer_head.sh
@@ -4,14 +4,13 @@ mkdir -p /data/waf/
 today=$(date +"%b_%d_%Y")
 log_file="/data/logs/ch_nohup_${today}.log"
 echo $log_file
+sleep 10
+
+# example with limited metadata loading
+
+#mvn -f ../consumers/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.consumers.coordinator.ConsumerCoordinator" -Dexec.args="-c cinergi-consumers-cfg-pipe-stage.xml -n 20 $*"
 
 #nohup mvn -q -f ../consumers/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.consumers.coordinator.ConsumerCoordinator" -Dexec.args="-i /home/cinergi/geo_time_include_list.txt -f $*" &> $log_file &
-
-
-#nohup mvn -q -f ../consumers/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.consumers.coordinator.ConsumerCoordinator" -Dexec.args="-f $*" &> $log_file
-
-#mvn -f ../consumers/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.consumers.coordinator.ConsumerCoordinator" -Dexec.args="-c cinergi-consumers-cfg-pipe-stage.xml -n 20 $*" 
-
 
 #java -cp foundry-consumers-1.0-SNAPSHOT-prod.jar org.neuinfo.foundry.consumers.coordinator.ConsumerCoordinator -c cinergi-consumers-cfg-pipe-stage.xml $*
 #nohup java -cp foundry-consumers-1.0-SNAPSHOT-prod.jar org.neuinfo.foundry.consumers.coordinator.ConsumerCoordinator -c cinergi-consumers-cfg-pipe-stage.xml -f &> $log_file &
--- foundry_stack/foundry/bin/docker_dispatcher.sh
+++ foundry_stack/foundry/bin/docker_dispatcher.sh
@@ -6,12 +6,11 @@ today=$(date +"%b_%d_%Y")
 mkdir -p /data/logs/
 log_file="/data/logs/dispatcher_nohup_${today}.log"
 echo $log_file
+sleep 10
+
 #nohup mvn -q -f ../dispatcher/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.jms.producer.PipelineMessageDispatcher"  &> $log_file
 
 #nohup mvn -q -f ../dispatcher/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.jms.producer.PipelineMessageDispatcher" -Dexec.args="-c cinergi-dispatcher-cfg-pipe-stage.xml" &> $log_file &
 
-#nohup mvn -f ../dispatcher/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.jms.producer.OplogMessageDispatcher" -Dexec.args="-c cinergi-dispatcher-cfg-pipe-stage.xml" &> $log_file &
-
-# java -cp foundry-dispatcher-1.0-SNAPSHOT-prod.jar org.neuinfo.foundry.jms.producer.OplogMessageDispatcher -c cinergi-dispatcher-cfg-pipe-stage.xml  $*
 # no config. pass in on entrypoint, if needed
 java -cp foundry-dispatcher-1.0-SNAPSHOT-prod.jar org.neuinfo.foundry.jms.producer.PipelineMessageDispatcher  $*
